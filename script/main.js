b82be7824990d78e3bfe2ffa7fc1b1c4a30dba6c
zeeshanmaqbool200 <zeeshanmaqbool200@gmail.com>
2024-08-31 14:55:21 +0530

    new

--- script/main.js
+++ script/main.js
@@ -10,23 +10,21 @@ function toggleNav(){
         mobileNav.style.display = "0";
         mobileNav.style.top = "-500px";
         document.body.style.overflow = "auto";
-<<<<<<< HEAD
         navBtn.innerText = "Menu";
-=======
->>>>>>> 0d12ab76b6b643988aea0ccc6bf5547f41eb33f3
     
     } else{
     mobileNav.style.left = "0px";
     mobileNav.style.top = "0px";
     mobileNav.style.gap = "5rem";
      mobileNav.style.height = "100vh";
-<<<<<<< HEAD
+     
     navBtn.style.zIndex = "9999";
      navBtn.innerText = "Close";
-=======
+
     navBtn.style.zIndex = "111";
+
      navBtn.style.background = "red";
->>>>>>> 0d12ab76b6b643988aea0ccc6bf5547f41eb33f3
+
        mobileNav.style.borderRadius = "0px";
        document.body.style.overflow = "hidden";
     }
