7e0093228ecee1671d58333c7c575eb14e3df348
zeeshanmaqbool200 <zeeshanmaqbool200@gmail.com>
2025-03-20 10:54:33 +0530

    gallery UI fix

--- script/main.js
+++ script/main.js
@@ -1,4 +1,13 @@
 const navBtn = document.getElementById("nav-btn");
 const mobileNav = document.getElementById("nav-right");
 const loader = document.querySelector('.background');
+const renderImg = document.getElementById('render-images');
 
+
+const gallery = 80;
+let htmlContent = ``;
+for (let i = 1; i <= gallery; i++) {
+    renderImg.innerHTML += `<div class="image"><img alt="Maktab Wali Ul Aser Image" src="/images/maktab/${i}.JPG"></div>`;
+
+    
+}
